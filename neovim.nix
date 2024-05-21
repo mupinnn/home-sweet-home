5900a919c97c757439aac9fe2b4de3f25dab1f94
Ahmad Muwaffaq <itsmupin@gmail.com>
2024-05-21 17:10:40 +0700

    feat(neovim): adjustment

--- neovim.nix
+++ neovim.nix
@@ -3,8 +3,8 @@
 {
   programs.neovim =
   let
-  	toLua = str: "lua << EOF\n${str}\nEOF\n";
-	toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+    toLua = str: "lua << EOF\n${str}\nEOF\n";
+    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
   in
   {
     enable = true;
@@ -14,15 +14,71 @@
     vimdiffAlias = true;
     
     plugins = with pkgs.vimPlugins; [
-    	{
-		plugin = comment-nvim;
-		config = toLua "require(\"Comment\").setup()";
-	}
+      {
+        plugin = lualine-nvim;
+        config = toLuaFile ./configs/nvim/lualine.lua;
+      }
 
-	{
-		plugin = gruvbox-nvim;
-		config = "colorscheme gruvbox";
-	}
+      nvim-web-devicons
+      {
+        plugin = nvim-tree-lua;
+        config = toLuaFile ./configs/nvim/nvim-tree.lua;
+      }
+
+      {
+        plugin = comment-nvim;
+        config = toLua "require('Comment').setup()";
+      }
+
+      {
+        plugin = indent-blankline-nvim;
+        config = toLua "require('ibl').setup()";
+      }
+
+      {
+        plugin = nvim-autopairs;
+        config= toLua "require('nvim-autopairs').setup()";
+      }
+
+      {
+        plugin = gitsigns-nvim;
+        config = toLua "require('gitsigns').setup()";
+      }
+
+      {
+        plugin = nvim-colorizer-lua;
+        config = toLua "require('colorizer').setup()";
+      }
+
+      {
+        plugin = (nvim-treesitter.withPlugins (p: [
+          p.tree-sitter-nix
+          p.tree-sitter-vim
+          p.tree-sitter-bash
+          p.tree-sitter-lua
+          p.tree-sitter-json
+          p.tree-sitter-css
+          p.tree-sitter-html
+          p.tree-sitter-javascript
+          p.tree-sitter-markdown
+          p.tree-sitter-scss
+          p.tree-sitter-tsx
+          p.tree-sitter-typescript
+          p.tree-sitter-vue
+          p.tree-sitter-yaml
+        ]));
+        config = toLuaFile ./configs/nvim/treesitter.lua;
+      }
+
+      {
+        plugin = telescope-nvim;
+        config = toLuaFile ./configs/nvim/telescope.lua;
+      }
+      telescope-fzf-native-nvim
     ];
+
+    extraLuaConfig = ''
+    	${builtins.readFile ./configs/nvim/options.lua}
+    '';
   };
 }
