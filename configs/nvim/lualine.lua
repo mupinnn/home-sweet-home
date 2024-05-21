1b1f820966bd799e0010767825b57c99eba62390
Ahmad Muwaffaq <itsmupin@gmail.com>
2024-05-21 17:09:20 +0700

    feat(configs/nvim): lualine

---
+++ configs/nvim/lualine.lua
@@ -0,0 +1,26 @@
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'papercolor_dark',
+    always_divide_middle = true,
+    globalstatus = true,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {
+      {
+        'branch',
+        icon = nil,
+      }
+    },
+    lualine_c = {'filename'},
+    lualine_x = {'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  }
+}
