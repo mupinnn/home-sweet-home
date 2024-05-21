a283629810564ba83e97bc92797a9abd6b841125
Ahmad Muwaffaq <itsmupin@gmail.com>
2024-05-21 17:09:03 +0700

    feat(configs/nvim): sensible defaults

---
+++ configs/nvim/options.lua
@@ -0,0 +1,11 @@
+vim.opt.title = true;
+vim.opt.number = true;
+vim.opt.relativenumber = true;
+vim.opt.shiftwidth = 2;
+vim.opt.syntax = "on";
+vim.opt.expandtab = true;
+vim.opt.softtabstop = 2;
+vim.opt.autoindent = true;
+vim.opt.smartindent = true;
+
+vim.cmd.colorscheme("habamax");
