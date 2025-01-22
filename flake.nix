6671becdec8ce97bc619f07d8fccd3ab6df5931e
Ahmad Muwaffaq <itsmupin@gmail.com>
2025-01-22 22:04:00 +0700

    refactor: initial usage of flakes (#2)
    * chore: initial flake setup
    
    * chore: first flake build

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "mupin's system configuration with Nix and `home-manager` using Flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOs/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+
+    homeConfigurations = {
+      mupin = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+        modules = [ ./home.nix ];
+      };
+    };
+  };
+}
