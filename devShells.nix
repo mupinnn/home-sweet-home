f946a1c37fb87b365e6499aa335f6a5971d1c69a
Ahmad Muwaffaq <itsmupin@gmail.com>
2025-02-04 16:42:05 +0700

    refactor(devShells): node.js defined versions

--- devShells.nix
+++ devShells.nix
@@ -1,11 +1,23 @@
 { pkgs, devenv, inputs, ... }:
 
-{
+let
+  mkNodeJsShell = name:
+    let node = pkgs.${name};
+    in pkgs.mkShell {
+      description =
+        "Node.js v${pkgs.lib.strings.getVersion node} development environment";
+      buildInputs = [ node pkgs.nodePackages.pnpm pkgs.bun ];
+    };
+
+  nodeJsShells = builtins.listToAttrs (map (nodeJsPkg: {
+    name = nodeJsPkg;
+    value = mkNodeJsShell nodeJsPkg;
+  }) [ "nodejs_18" "nodejs_20" "nodejs_22" ]);
+in {
   php83 = devenv.lib.mkShell {
     inherit inputs pkgs;
 
     modules = [{
-
       languages.php = {
         enable = true;
         package = pkgs.php83;
@@ -17,9 +29,4 @@
     description = "C/C++ development environment";
     buildInputs = with pkgs; [ gcc13 clang clang-tools cmake gnumake ];
   };
-
-  nodejs20 = pkgs.mkShell {
-    description = "Node.js 20 development environment";
-    buildInputs = with pkgs; [ nodejs_20 ];
-  };
-}
+} // nodeJsShells
