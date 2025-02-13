1daf1ae32b0b841f06a8e32514318ea95e6ce0e5
Ahmad Muwaffaq <itsmupin@gmail.com>
2025-02-13 21:17:46 +0700

    feat(nixvim): `cssls` lsp capabilities

--- nixvim.nix
+++ nixvim.nix
@@ -400,6 +400,14 @@ in {
           ts_ls.enable = true;
           volar.enable = true;
         };
+        capabilities = ''
+          local capabilities = vim.lsp.protocol.make_client_capabilities()
+          capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+          require("lspconfig").cssls.setup {
+            capabilities = capabilities
+          }
+        '';
       };
 
       conform-nvim = {
